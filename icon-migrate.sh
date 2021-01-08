f56240b81586e902e582357e0870f4891bff986a
Tim Fletcher <timfletcher88@gmail.com>
2021-01-08 12:53:12 +1100

    Added script to run Icon Agency's migrations - https://github.com/invisigoth/dva-migrate/tree/master/web/modules/custom/icon_migrate

---
+++ icon-migrate.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+#   //////////////////////////////////
+#  // Run Icon Agency's migrations //
+# //////////////////////////////////
+#
+
+START=`date +%s`
+
+echo -e "\n\n***************\nRunning a fresh migration"
+
+echo -e "\n-------------\nEnabling required modules..."
+drush en -y migrate_plus migrate_drupal migrate_tools migrate_upgrade media_migration field_group field_group_migrate
+
+echo -e "\n-------------\nImporting Icon migration configuration"
+drush config-import -y --partial --source=modules/custom/icon_migrate/config/install
+
+echo -e "\n-------------\nMigrating Configuration"
+drush migrate:import --group=icon_migrate --tag=Configuration --execute-dependencies --continue-on-failure
+
+echo -e "\n-------------\nMigrating Files and assets"
+drush migrate:import --group=icon_migrate --tag=Asset --execute-dependencies --continue-on-failure
+
+echo -e "\n-------------\nMigrating Nodes (this will take a while)"
+drush migrate:import --group=icon_migrate --tag=Node --execute-dependencies
+
+echo -e "\n-------------\nMigrating Menus"
+drush migrate:import --group=icon_migrate --tag=Menu --execute-dependencies
+
+echo -e "\n-------------\nEnabling Icon Migrate Blocks module"
+drush en -y icon_migrate_block
+
+echo -e "\n-------------\nMigrating Blocks"
+drush migrate:import --group=icon_migrate_block --tag=Block
+
+
+END=`date +%s`
+TIMER_SECONDS=$((END-START))
+
+printf 'Check completed in %02dh:%02dm:%02ds\n' $(($TIMER_SECONDS/3600)) $(($TIMER_SECONDS%3600/60)) $(($TIMER_SECONDS%60))
+
+echo -e "\n\n*********** Migration complete ***************\n\n"
