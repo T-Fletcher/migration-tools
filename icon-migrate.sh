b4fcc7b1dd8b8c1e280c8b6a266cf6f438481aaf
Tim Fletcher <timfletcher88@gmail.com>
2021-01-13 17:06:14 +1100

    Added a script to run the entire Icon migration

--- icon-migrate.sh
+++ icon-migrate.sh
@@ -9,8 +9,12 @@ START=`date +%s`
 
 echo -e "\n\n***************\nRunning a fresh migration"
 
+echo -e "\n-------------\nReplacing database with fresh template..."
+drush sql-drop -y
+drush sql-cli < /Users/tim/repositories/dva-d9/localhost-dvamigrate-empty-site.sql
+
 echo -e "\n-------------\nEnabling required modules..."
-drush en -y migrate_plus migrate_drupal migrate_tools migrate_upgrade media_migration field_group field_group_migrate
+drush en -y migrate_plus migrate_drupal migrate_tools migrate_upgrade media_migration smart_sql_idmap field_group field_group_migrate
 
 echo -e "\n-------------\nImporting Icon migration configuration"
 drush config-import -y --partial --source=modules/custom/icon_migrate/config/install
@@ -37,6 +41,6 @@ drush migrate:import --group=icon_migrate_block --tag=Block
 END=`date +%s`
 TIMER_SECONDS=$((END-START))
 
-printf 'Check completed in %02dh:%02dm:%02ds\n' $(($TIMER_SECONDS/3600)) $(($TIMER_SECONDS%3600/60)) $(($TIMER_SECONDS%60))
+printf 'Migration completed in %02dh:%02dm:%02ds\n' $(($TIMER_SECONDS/3600)) $(($TIMER_SECONDS%3600/60)) $(($TIMER_SECONDS%60))
 
 echo -e "\n\n*********** Migration complete ***************\n\n"
